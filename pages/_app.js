0c29552750c0b43f2c38cc02b31c6083618056fe
Ciro Cativo <cirocativo@gmail.com>
2024-12-06 05:46:07 +0000

    feat: surpreendendo a amada

---
+++ pages/_app.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import './styles.css';
+import family from '../assets/family.jpeg';
+import home from '../assets/home.jpg';
+import keroarts from '../assets/keroarts.jpg';
+import Image from 'next/image';
+
+export default function Home() {
+    const [clicked, setClicked] = useState(false);
+    const [position, setPosition] = useState(0);
+
+    const imageList = [home, family, keroarts];
+
+    useEffect(() =>{
+        if(!clicked) return;
+        
+        if(position >= imageList.length){
+            setClicked(false);
+            setPosition(0);
+            return;
+        }
+        setTimeout(() => {  
+            setPosition(position+1);
+        }, 3000);
+
+    },[position, clicked]);
+
+    return(
+    <div className="main">
+        {!clicked ?
+        <>
+            <h1>Olá mundo!</h1>
+            <div className='button' onClick={() => setClicked(true)}>Clique aqui para ver o futuro acontecer</div>
+        </>
+        :
+        <div className='image-container'>
+            <Image src={imageList[position]}  onClick={() => setClicked(false)}/>
+        </div>
+    }
+    </div>
+    )
+}
